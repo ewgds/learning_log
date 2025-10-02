e6b9df38f7f068982ffcbdecdcf525c54a54d01e
sergey <sergey@localhost.localdomain>
2025-10-02 09:42:38 +0300

    added function for editing entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     # Adding new topic page
     path('new_topic/', views.new_topic, name='new_topic'),
     # Adding new entries page
-    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Edit entry 
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry')
 ]
